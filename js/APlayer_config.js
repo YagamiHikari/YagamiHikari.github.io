b1a35b9199d25e678f3b6c74fe5a402ddb6cf110
YagamiHikari <297135187@qq.com>
2020-07-21 07:22:31 +0800

    Site updated: 2020-07-21 07:21:50

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -12,11 +12,11 @@ const ap = new APlayer({
     listFolded: false,
 
     audio: [{
-            name: '一直很安静',
-            artist: '阿桑',
-            lrc: '/music/lrc/一直很安静 - 阿桑.lrc',
+            name: 'せつなくて (悲伤) (5 Voice_Acoustic ver.)',
+            artist: 'Da-iCE',
+            lrc: '/music/lrc/Da-iCE - せつなくて (悲伤) (5 Voice_Acoustic ver.).lrc',
             cover: 'https://p1.music.126.net/SpovasHBud2A1qXXADXsBg==/109951163167455610.jpg?param=300x300',
-            url: 'http://q0fzyzixq.bkt.clouddn.com/audio/mp3/一直很安静 - 阿桑.mp3'
+            url: 'https://warmblog.oss-cn-beijing.aliyuncs.com/Da-iCE%20-%20%E3%81%9B%E3%81%A4%E3%81%AA%E3%81%8F%E3%81%A6%20%28%E6%82%B2%E4%BC%A4%29%20%285%20Voice_Acoustic%20ver.%29.mp3'
         },
         {
             name: '亲爱的旅人啊（Cover：木村弓）',
