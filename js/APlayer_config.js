452ac8b644f94dcadbc0a0502f8c5f7149d4af7d
YagamiHikari <297135187@qq.com>
2020-07-21 08:39:47 +0800

    Site updated: 2020-07-21 08:39:06

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -14,16 +14,16 @@ const ap = new APlayer({
     audio: [{
             name: 'せつなくて (悲伤)(5 Voice_Acoustic ver.)',
             artist: 'Da-iCE',
-            lrc: 'https://warmblog.oss-cn-beijing.aliyuncs.com/Da-iCE%20-%20%E3%81%9B%E3%81%A4%E3%81%AA%E3%81%8F%E3%81%A6%20%28%E6%82%B2%E4%BC%A4%29%20%285%20Voice_Acoustic%20ver.%29.lrc',
+            lrc: '/music/lrc/Da-iCE - せつなくて (悲伤) (5 Voice_Acoustic ver.).lrc',
             cover: '/music/lrc/Da-iCE - せつなくて (悲伤) (5 Voice_Acoustic ver.).jpg',
-            url: 'https://warmblog.oss-cn-beijing.aliyuncs.com/Da-iCE%20-%20%E3%81%9B%E3%81%A4%E3%81%AA%E3%81%8F%E3%81%A6%20%28%E6%82%B2%E4%BC%A4%29%20%285%20Voice_Acoustic%20ver.%29.mp3'
+            url: '/music/lrc/Da-iCE - せつなくて (悲伤) (5 Voice_Acoustic ver.).mp3'
         },
         {
             name: '夏草や、去りし日々の影法師',
             artist: 'FELT',
             lrc: '/music/lrc/FELT - 夏草や、去りし日々の影法師.lrc',
             cover: '/music/lrc/FELT - 夏草や、去りし日々の影法師.jpg',
-            url: 'https://warmblog.oss-cn-beijing.aliyuncs.com/FELT%20-%20%E5%A4%8F%E8%8D%89%E3%82%84%E3%80%81%E5%8E%BB%E3%82%8A%E3%81%97%E6%97%A5%E3%80%85%E3%81%AE%E5%BD%B1%E6%B3%95%E5%B8%AB.mp3'
+            url: '/music/lrc/FELT - 夏草や、去りし日々の影法師.mp3'
         },
         {
             name: '好きでした。 (曾经喜欢过。)',
